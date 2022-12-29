e4de005e6290bab9a470b01029331c079d08f364
PSGitHubUser1 <psmailuser.2021@gmail.com>
2022-12-29 21:52:44 +0530

    Added 6 more paintings

--- src/main/java/net/celebration/christmas/init/ChristmasModPaintings.java
+++ src/main/java/net/celebration/christmas/init/ChristmasModPaintings.java
@@ -20,5 +20,12 @@ public class ChristmasModPaintings {
 		Registry.register(Registry.PAINTING_VARIANT, new ResourceLocation(ChristmasMod.MODID, "christmas_pointer"), new PaintingVariant(64, 64));
 		Registry.register(Registry.PAINTING_VARIANT, new ResourceLocation(ChristmasMod.MODID, "christmas_pigscene"), new PaintingVariant(64, 64));
 		Registry.register(Registry.PAINTING_VARIANT, new ResourceLocation(ChristmasMod.MODID, "christmas_match"), new PaintingVariant(32, 32));
+		Registry.register(Registry.PAINTING_VARIANT, new ResourceLocation(ChristmasMod.MODID, "christmas_burning_skull"),
+				new PaintingVariant(64, 64));
+		Registry.register(Registry.PAINTING_VARIANT, new ResourceLocation(ChristmasMod.MODID, "christmas_bust"), new PaintingVariant(32, 32));
+		Registry.register(Registry.PAINTING_VARIANT, new ResourceLocation(ChristmasMod.MODID, "christmas_creebet"), new PaintingVariant(32, 16));
+		Registry.register(Registry.PAINTING_VARIANT, new ResourceLocation(ChristmasMod.MODID, "christmas_fighters"), new PaintingVariant(64, 32));
+		Registry.register(Registry.PAINTING_VARIANT, new ResourceLocation(ChristmasMod.MODID, "christmas_graham"), new PaintingVariant(16, 32));
+		Registry.register(Registry.PAINTING_VARIANT, new ResourceLocation(ChristmasMod.MODID, "christmas_wanderer"), new PaintingVariant(16, 32));
 	}
 }
