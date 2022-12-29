c8cfe4712177e338e99c1d05e1140fc2d3ddb920
PSGitHubUser1 <psmailuser.2021@gmail.com>
2022-12-29 22:14:37 +0530

    Fixed recipes and made a TODO list for me

---
+++ src/main/java/net/celebration/christmas/TodoList.java
@@ -0,0 +1,18 @@
+
+package net.celebration.christmas;
+public class TodoList {
+
+/* How you are seeing this code? Idea/Eclipse? Its my private file, 
+ *  This is not an element.
+ * This is just TODO list which I've to follow every time whenever 
+ * Before I publish a new release.
+ *
+ * -----------------------------------------------------------------
+ */
+
+// 1. Check the recipes' registry names [*]
+// 2. Regen the code [_]
+// 3. Structure codes [-]
+
+
+}
